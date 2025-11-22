9652226751f5ddca1dc1e10d5aad30184d7ab016
jeyellll <153403094+jeyellll@users.noreply.github.com>
2025-11-22 07:18:32 +0000

    Update message

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to my page.");
+  res.write("Arcena, Jodie Lance A. , ");
+  res.write("BSIT BA-4102 , ");
+  res.write("“In the midst of chaos, there is also opportunity”");
   res.end();
 });
 
